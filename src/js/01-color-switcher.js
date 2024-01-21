e72233f221eb19c19457592b7a5e51b427beeaba
OlhaOs <olalaadka@gmail.com>
2024-01-21 16:59:38 +0200

    first task

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,31 @@
+const refs = {
+  bodyEl: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', handleStartBtn);
+refs.stopBtn.addEventListener('click', handleStopBtn);
+
+let timerId;
+
+function handleStartBtn() {
+  refs.startBtn.disabled = true;
+  setBodyColor();
+  timerId = setInterval(setBodyColor, 1000);
+}
+
+function setBodyColor() {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+function handleStopBtn() {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
